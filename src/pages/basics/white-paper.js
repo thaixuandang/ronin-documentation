15c4c500bf1e32880a60d0904408afd56443da49
Sergey Rodin <sergeyrodin.sr@gmail.com>
2024-06-19 09:27:18 +0700

    feat: add Download PDF button for whitepaper

--- src/pages/basics/white-paper.js
+++ src/pages/basics/white-paper.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import './white-paper.css'; // Import the CSS file for custom styling
 
 const WhitePaper = () => {
+  const googleDriveEmbedLink = "https://drive.google.com/file/d/1pZoUWvbT45-G7zcD2XThPFFPDrUGeqYc/preview";
+  const googleDriveDownloadLink = "https://drive.google.com/uc?export=download&id=1pZoUWvbT45-G7zcD2XThPFFPDrUGeqYc";
+
   return (
-    <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div className="pdf-container">
       <iframe
-        src="https://drive.google.com/file/d/1pZoUWvbT45-G7zcD2XThPFFPDrUGeqYc/preview"
+        src={googleDriveEmbedLink}
         width="100%"
         height="100%"
         style={{ border: 'none' }}
       ></iframe>
+      <a href={googleDriveDownloadLink} className="download-button" download>
+        Download PDF
+      </a>
     </div>
   );
 };
