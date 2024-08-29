01c0a524a8876d2e0534ee134b348b1ef9533b1a
Sergey Rodin <sergeyrodin.sr@gmail.com>
2024-08-29 12:35:55 +0700

    chore: site name and banner heading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,10 @@ import styles from './index.module.scss'
 
 const Home: React.FC = () => {
   return (
-    <Layout title={`Home`} description='Ronin Chain Documentation'>
+    <Layout title={`Home`} description='Ronin Docs'>
       <div className={styles.background}>
         <header className={styles.heroBanner}>
-          <h1 className={styles.heroBannerHeading}>Welcome to the Ronin chain documentation</h1>
+          <h1 className={styles.heroBannerHeading}>Ronin chain documentation for delegators, validators, and bridge operators</h1>
         </header>
         <main className={styles.main}>
           <div>
